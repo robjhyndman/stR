e44e86d2cee5cd39d971621ea17df37891b282de
alex <alexander.dokumentov@gmail.com>
2016-07-04 00:14:28 +1000

    Forgotten file for the previous commit.

--- stR/testSTR.R
+++ stR/testSTR.R
@@ -68,6 +68,33 @@ plot(str)
 
 #############################################
 
+n = 70
+trendSeasonalStructure = list(segments = list(c(0,1)), sKnots = list(c(1,0)))
+ns = 5
+seasonalStructure = list(segments = list(c(0,ns)), sKnots = c(as.list(1:(ns-1)),list(c(ns,0))))
+seasons = (0:(n-1))%%ns + 1
+trendSeasons = rep(1, length(seasons))
+times = seq_along(seasons)
+data = seasons + times/4
+set.seed(1234567890)
+data = data + rnorm(length(data), 0, 0.2)
+data[20] = data[20]+3
+data[50] = data[50]-5
+plot(times, data, type = "l")
+timeKnots = times
+trendData = rep(1, n)
+seasonData = rep(1, n)
+trend = list(data = trendData, times = times, seasons = trendSeasons, timeKnots = timeKnots, seasonalStructure = trendSeasonalStructure, lambdas = c(1,0,0))
+season = list(data = seasonData, times = times, seasons = seasons, timeKnots = timeKnots, seasonalStructure = seasonalStructure, lambdas = c(1,0,1))
+predictors = list(trend, season)
+rstr = RSTR(data, predictors, confidence = 0.8)
+plot(rstr)
+
+rstr2 = AutoRSTR(data, predictors, reltol = 0.0000001, gapCV = 10, confidence = 0.95, nMCIter = 400)
+plot(rstr2)
+
+#############################################
+
 mydata <- read.csv("Seasonal_data.csv")
 ts1 = ts(BoxCox(mydata[,2], BoxCox.lambda(mydata[,2])), start = c(1982,4), frequency = 12)
 ts2 = window(ts1, start = c(2000,1), end = c(2009,12))
