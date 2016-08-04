4753587af33db86cb1cfc82c27cb2bcf09e3c67f
alex <alexander.dokumentov@gmail.com>
2016-08-05 01:14:38 +1000

    Datasets introduced. More documentation.

---
+++ stR/R/grocery.R
@@ -0,0 +1,20 @@
+#' Grocery turnover dataset
+#'
+#' Turnover of Supermarkets and grocery stores in New South Wales.
+#'
+#' @docType data
+#'
+#' @usage grocery
+#'
+#' @format An object of class \code{ts}.
+#'
+#' @keywords datasets
+#'
+#' @references Australian Bureau of Statistics
+#' (\href{http://www.abs.gov.au/}{ABS})
+#'
+#' @source \href{http://search.abs.gov.au/s/redirect?rank=1&collection=abs&url=http%3A%2F%2Fwww.ausstats.abs.gov.au%2Fausstats%2Fmeisubs.nsf%2F0%2F3E8664D192E068EFCA2580040013D2B5%2F%24File%2F8501012.xls&index_url=http%3A%2F%2Fwww.ausstats.abs.gov.au%2Fausstats%2Fmeisubs.nsf%2F0%2F3E8664D192E068EFCA2580040013D2B5%2F%24File%2F8501012.xls&auth=cfeKpFOfASTOIc5LGQnDWw&query=Turnover+%3B++New+South+Wales+%3B++Supermarket+and+grocery+stores&profile=_default_preview}
+#'
+#' @examples
+#' plot(grocery, ylab = "NSW Grocery, $ 10^6")
+"grocery"
