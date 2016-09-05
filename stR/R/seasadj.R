d29d52854c2b9a9c9050520b02be3a60eff868b7
dokal01 <adokumen@AD.MONASH.EDU>
2016-09-05 14:54:10 +1000

    Initial implementation of method "seasadj". To be updated after the corresponding function in the forecast package is made S3 method.

--- stR/R/seasadj.R
+++ stR/R/seasadj.R
@@ -5,18 +5,41 @@
 #' @description \code{seasadj.STR} extracts seasonally adjusted data by removing the seasonal components from the result of STR decomposition.
 #' @seealso \code{\link{STRmodel}}, \code{\link{RSTRmodel}}, \code{\link{STR}}, \code{\link{AutoSTR}}
 #' @param object Result of STR decomposition.
+#' @param include Vecor of component names to include in the result. The default is \code{c("Trend", "Random")}.
 #' @author Alexander Dokumentov
 #' @examples
 #' \dontrun{
 #'
 #' fit <- AutoSTR(log(grocery))
-#' plot(seasadj(fit))
+#' plot(stR::seasadj(fit))
 #'
 #' }
 #' @export
 
-seasadj.STR = function(object)
+seasadj.STR = function(object, include = c("Trend", "Random"))
 {
-  return(NULL)
+  compTs = components(object)
+  trendName = colnames(compTs)[2]
+  if(is.null(trendName) || is.na(trendName) || nchar(trendName) == 0) {
+    warning("Trend component is not specified by name, using the first component as the Trend component.")
+    colnames(compTs)[2] = "Trend"
+  }
+  for(name in include[!(include %in% colnames(compTs))]) {
+    warning(paste(name, "is not one of the components of the decomposion, skipping..."))
+  }
+  result = NULL
+  for(i in include[include %in% colnames(compTs)]) {
+    if(is.null(result)) {
+      result = compTs[,i]
+    } else {
+      result = result + compTs[,i]
+    }
+  }
+  return(result)
 }
+
+#' @name seasadj
+#' @rdname seasadj.STR
+#' @export seasadj
+seasadj <- function(object, ...) UseMethod("seasadj", object)
 
