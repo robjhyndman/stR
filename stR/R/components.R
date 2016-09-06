0ac7ca4c69cf60bbdbd837e969fcd228a1d6d06a
robjhyndman <robjhyndman@gmail.com>
2016-09-06 14:06:11 +1000

    Added comments to components function

--- stR/R/components.R
+++ stR/R/components.R
@@ -1,43 +1,47 @@
 #' @importFrom stats tsp
+#' @importFrom stats tsp<-
 #' @importFrom stats ts
 
 #' @name components
 #' @rdname components
 #'
-#' @title Extracts components as time series from the result of decomposition
-#' @description \code{components} extracts components as time series from the result of STR decomposition.
+#' @title Extract STR components
+#' @description \code{components} extracts components as time series from the result of an STR decomposition.
 #' @seealso \code{\link{STRmodel}}, \code{\link{RSTRmodel}}, \code{\link{STR}}, \code{\link{AutoSTR}}
 #' @param object Result of STR decomposition.
 #' @author Alexander Dokumentov
 #' @examples
 #' \dontrun{
-#'
+#' 
 #' fit <- AutoSTR(log(grocery))
-#' comp = components(fit)
+#' comp <- components(fit)
 #' plot(comp)
-#'
 #' }
 #' @export
 
-components = function(object)
+components <- function(object)
 {
-  l = length(object$input$data)
-  n = length(object$output$predictors) + 2 # predictors, original data and random component
-  m = matrix(0, l, n)
-  m[,1] = as.vector(object$input$data)
-  m[,ncol(m)] = as.vector(object$output$random$data)
-  names = rep("", ncol(m))
-  names[c(1, ncol(m))] = c("Original", "Random")
+  # Set up matrix for components
+  l <- length(object$input$data)
+  n <- length(object$output$predictors) + 2 # predictors, original data and random component
+  m <- matrix(0, l, n)
+
+  # Add data to first column and random to last column
+  m[,1] <- as.vector(object$input$data)
+  m[,ncol(m)] <- as.vector(object$output$random$data)
+  names <- rep("", ncol(m))
+  names[c(1, ncol(m))] = c("Data", "Random")
+
+  # Add remaining components
   for(i in seq_along(object$output$predictors)) {
-    m[,i+1] = object$output$predictors[[i]]$data
-    names[i+1] = object$input$predictors[[i]]$name
+    m[,i+1] <- object$output$predictors[[i]]$data
+    names[i+1] <- object$input$predictors[[i]]$name
   }
-  colnames(m) = names
-  if("ts" %in% class(object$input$data)) {
-    p = tsp(object$input$data)
-    result = ts(m, start = p[1], end = p[2], frequency = p[3])
-  } else {
-    result = ts(m)
-  }
-  return(result)
+  colnames(m) <- names
+
+  # Return result with ts attributes
+  m <- ts(m)
+  if("ts" %in% class(object$input$data))
+    tsp(m) <- tsp(object$input$data)
+  return(m)
 }
