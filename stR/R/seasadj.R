2393a87bad200c36966c70a5808263c131395c7d
robjhyndman <robjhyndman@gmail.com>
2016-09-06 14:07:00 +1000

    Added comments to seasadj function

--- stR/R/seasadj.R
+++ stR/R/seasadj.R
@@ -1,45 +1,55 @@
 #' @name seasadj.STR
 #' @rdname seasadj.STR
 #'
-#' @title Seasonal adjustment
+#' @title Seasonal adjustment based on STR
 #' @description \code{seasadj.STR} extracts seasonally adjusted data by removing the seasonal components from the result of STR decomposition.
 #' @seealso \code{\link{STRmodel}}, \code{\link{RSTRmodel}}, \code{\link{STR}}, \code{\link{AutoSTR}}
 #' @param object Result of STR decomposition.
-#' @param include Vecor of component names to include in the result. The default is \code{c("Trend", "Random")}.
+#' @param include Vector of component names to include in the result. The default is \code{c("Trend", "Random")}.
+#' @param ... Other arguments not currently used.
 #' @author Alexander Dokumentov
 #' @examples
 #' \dontrun{
-#'
+#' 
 #' fit <- AutoSTR(log(grocery))
-#' plot(stR::seasadj(fit))
-#'
+#' plot(seasadj(fit))
 #' }
 #' @export
 
-seasadj.STR = function(object, include = c("Trend", "Random"))
+seasadj.STR = function(object, include = c("Trend", "Random"), ...)
 {
-  compTs = components(object)
-  trendName = colnames(compTs)[2]
+  # Extract all components
+  compTs <- components(object)
+
+  # Find trend
+  trendName <- colnames(compTs)[2]
   if(is.null(trendName) || is.na(trendName) || nchar(trendName) == 0) {
     warning("Trend component is not specified by name, using the first component as the Trend component.")
-    colnames(compTs)[2] = "Trend"
+    colnames(compTs)[2] <- "Trend"
   }
+
+  # Check all components are available
   for(name in include[!(include %in% colnames(compTs))]) {
     warning(paste(name, "is not one of the components of the decomposion, skipping..."))
   }
-  result = NULL
+
+  # Add together the components listed in include argument.
+  result <- NULL
   for(i in include[include %in% colnames(compTs)]) {
     if(is.null(result)) {
-      result = compTs[,i]
+      result <- compTs[,i]
     } else {
-      result = result + compTs[,i]
+      result <- result + compTs[,i]
     }
   }
+
+  # Return result
   return(result)
 }
 
 #' @name seasadj
 #' @rdname seasadj.STR
 #' @export seasadj
-seasadj <- function(object, ...) UseMethod("seasadj", object)
+seasadj <- function(object, ...) 
+  UseMethod("seasadj", object)
 
