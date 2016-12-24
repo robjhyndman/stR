27777fee97dbc3b851a5cf7f6ba4ece015cf12be
dokal01 <alexander.dokumentov@gmail.com>
2016-12-25 01:03:43 +1100

    heuristicSTR is implemented. The method can provide a better estimate of lambda parameters, especially in case when initial estimate is too far from the optimal set of the parameters.

--- stR/R/heuristicSTR.R
+++ stR/R/heuristicSTR.R
@@ -1,7 +1,8 @@
 #' @rdname heuristicSTR
 #' @name heuristicSTR
 #' @title Automatic STR decomposition with heuristic hyperparameters search
-#' @description Automatically selects parameters for an STR decomposition of time series data.
+#' @description Automatically selects hyperparameters (lambda coefficients) for an STR decomposition of time series data.
+#' Heuristic approach can give a better estimate compare to a standard optmisaton methods used in \code{\link{STR}}.
 #'
 #' If a parallel backend is registered for use before \code{STR} call,
 #' \code{heuristicSTR} will use it for n-fold cross validation computations.
@@ -17,6 +18,8 @@
 #' @inheritParams gapCV
 #' @inheritParams solver
 #' @inheritParams trace
+#' @param ratioGap Ratio to define hyperparameter bounds for one-dimensional search.
+#' @param relCV Minimum improvement required after all predictors tried. It is used to exit heuristic serach of lambda parameters.
 #' @templateVar class STR
 #' @templateVar topLevel1 \item \strong{cvMSE} -- optional cross validated (leave one out) Mean Squared Error.
 #' @templateVar topLevel2 \item \strong{optim.CV.MSE} or \strong{optim.CV.MAE} -- best cross validated Mean Squared Error or Mean Absolute Error (n-fold) achieved during minimisation procedure.
@@ -96,7 +99,7 @@ heuristicSTR = function(data, predictors,
                         pattern = stR:::extractPattern(predictors), nFold = 5, reltol = 0.005, gapCV = 1,
                         solver = c("Matrix", "cholesky"),
                         trace = FALSE,
-                        ratioGap = 1e6, # Ratio to define bounds for one-dimensional search
+                        ratioGap = 1e6, # Ratio to define bounds for one-dimensional search.
                         relCV = 0.01 # Minimum improvement required after all predictors tried. It is used to exit.
 )
 {
@@ -104,7 +107,7 @@ heuristicSTR = function(data, predictors,
                             confidence = confidence, lambdas = lambdas,
                             pattern = pattern, nFold = nFold,
                             reltol = reltol*10, gapCV = gapCV,
-                            solver = solver, trace = trace)
+                            solver = solver, trace = trace, ratioGap = ratioGap)
     result1 = STR(data = data, predictors = predictors,
                   confidence = confidence, lambdas = lambdas,
                   pattern = pattern, nFold = nFold,
@@ -114,7 +117,7 @@ heuristicSTR = function(data, predictors,
                             confidence = confidence, lambdas = result1$input$lambdas,
                             pattern = pattern, nFold = nFold,
                             reltol = reltol*10, gapCV = gapCV,
-                            solver = solver, trace = trace)
+                            solver = solver, trace = trace, relCV = relCV, ratioGap = ratioGap)
     result2 = STR(data = data, predictors = predictors,
                   confidence = confidence, lambdas = lambdas,
                   pattern = pattern, nFold = nFold,
@@ -173,14 +176,18 @@ upDownLambdas = function(data, predictors,
   if(trace) { cat("\nCV: "); cat(newCV) }
 
   loopCV = newCV
+  fit = NULL
   while(TRUE) {
     for(i in seq_along(predictors)) {
+      if(sum(abs(newLambdas[[i]]$lambdas)) == 0) next
       if(trace) { cat("\n\nPredictor: "); cat(i) }
       if(trace) {
         cat("\n")
         dummy = lapply(newLambdas, FUN = function(p) {cat(format(p$lambdas, digits = 4, scientific = T, width = 9)); cat("  ")})
       }
-      fit = STRmodel(data, strDesign = strDesign, lambdas = newLambdas, confidence = NULL, trace = F)
+      if(is.null(fit)) {
+        fit = STRmodel(data, strDesign = strDesign, lambdas = newLambdas, confidence = NULL, trace = F)
+      }
       newData = fit$output$predictors[[i]]$data + fit$output$random$data
       newPredictors = fit$input$predictors[i]
       startLambdas = newLambdas[i]
@@ -208,6 +215,7 @@ upDownLambdas = function(data, predictors,
       if(candidateCV < newCV) {
         if(trace) { cat("\nCandidate is better") }
         newLambdas = candidateLambdas
+        fit = NULL
         newCV = candidateCV
         if(trace) {
           cat("\n")
@@ -257,12 +265,19 @@ upLambdas = function(data, predictors,
   regSeats = rm$seats
 
   newLambdas = lambdas
-  if(trace) dummy = lapply(newLambdas, FUN = function(p) {cat(format(p$lambdas, digits = 4, scientific = T, width = 9)); cat("  ")})
+  if(trace) {
+    dummy = lapply(newLambdas, FUN = function(p) {cat(format(p$lambdas, digits = 4, scientific = T, width = 9)); cat("  ")})
+  }
+  fit = NULL
   for(j in seq_len(nPasses)) {
     for(i in seq_along(predictors)) {
-      if(trace) cat("\nPredictor: "); cat(i); cat("\n")
-      # dummy = lapply(newLambdas, FUN = function(p) {cat(format(p$lambdas, digits = 4, scientific = T, width = 9)); cat("  ")}); cat("\n")
-      fit = STRmodel(data, strDesign = strDesign, lambdas = newLambdas, confidence = NULL, trace = F)
+      if(sum(abs(newLambdas[[i]]$lambdas)) == 0) next
+      if(trace) {
+        cat("\nPredictor: "); cat(i); cat("\n")
+      }
+      if(is.null(fit)) {
+        fit = STRmodel(data, strDesign = strDesign, lambdas = newLambdas, confidence = NULL, trace = F)
+      }
       newData = fit$output$predictors[[i]]$data + fit$output$random$data
       newPredictors = fit$input$predictors[i]
       startLambdas = newLambdas[i]
@@ -283,8 +298,11 @@ upLambdas = function(data, predictors,
         next
       }
       else {
-        newLambdas[[i]]$lambdas = fit_$input$lambdas[[1]]$lambdas # It can be a formula like: l = alpha*estimatedL + (1-alpha)*l
-        if(trace) dummy = lapply(newLambdas, FUN = function(p) {cat(format(p$lambdas, digits = 4, scientific = T, width = 9)); cat("  ")})
+        newLambdas[[i]]$lambdas = fit_$input$lambdas[[1]]$lambdas
+        fit = NULL
+        if(trace) {
+          dummy = lapply(newLambdas, FUN = function(p) {cat(format(p$lambdas, digits = 4, scientific = T, width = 9)); cat("  ")})
+        }
       }
     }
   }
@@ -330,3 +348,71 @@ upLambdas = function(data, predictors,
 #
 # plot(elec.fit,
 #      xTime = as.Date("2000-01-11") + ((Times-1)/48-10))
+
+# TrendSeasonalStructure <- list(segments = list(c(0,1)),
+# sKnots = list(c(1,0)))
+# WDSeasonalStructure <- list(segments = list(c(0,48), c(100,148)),
+#                             sKnots = c(as.list(c(1:47,101:147)), list(c(0,48,100,148))))
+#
+# TrendSeasons <- rep(1, nrow(electricity))
+# WDSeasons <- as.vector(electricity[,"WorkingDaySeasonality"])
+#
+# Data <- as.vector(electricity[,"Consumption"])
+# Times <- as.vector(electricity[,"Time"])
+# TempM <- as.vector(electricity[,"Temperature"])
+# TempM2 <- TempM^2
+#
+# TrendTimeKnots <- seq(from = head(Times, 1), to = tail(Times, 1), length.out = 116)
+# SeasonTimeKnots <- seq(from = head(Times, 1), to = tail(Times, 1), length.out = 24)
+#
+# TrendData <- rep(1, length(Times))
+# SeasonData <- rep(1, length(Times))
+#
+# Trend <- list(name = "Trend",
+#              data = TrendData,
+#              times = Times,
+#              seasons = TrendSeasons,
+#              timeKnots = TrendTimeKnots,
+#              seasonalStructure = TrendSeasonalStructure,
+#              lambdas = c(1500,0,0))
+# WDSeason <- list(name = "Dayly seas",
+#                 data = SeasonData,
+#                 times = Times,
+#                 seasons = WDSeasons,
+#                 timeKnots = SeasonTimeKnots,
+#                 seasonalStructure = WDSeasonalStructure,
+#                 lambdas = c(0.003,0,240))
+# StaticTempM <- list(name = "Temp Mel",
+#                    data = TempM,
+#                    times = Times,
+#                    seasons = NULL,
+#                    timeKnots = NULL,
+#                    seasonalStructure = NULL,
+#                    lambdas = c(0,0,0))
+# StaticTempM2 <- list(name = "Temp Mel^2",
+#                     data = TempM2,
+#                     times = Times,
+#                     seasons = NULL,
+#                     timeKnots = NULL,
+#                     seasonalStructure = NULL,
+#                     lambdas = c(0,0,0))
+# Predictors <- list(Trend, WDSeason, StaticTempM, StaticTempM2)
+#
+#
+# tm = system.time({
+#   elec.fit <- heuristicSTR(data = Data,
+#                            predictors = Predictors,
+#                            gapCV = 48*7,
+#                            trace = TRUE)
+# }); print(tm)
+#
+# tm = system.time({
+#   elec.fit <- STR(data = Data,
+#                   predictors = Predictors,
+#                   gapCV = 48*7,
+#                   trace = TRUE)
+# }); print(tm)
+#
+# plot(elec.fit,
+#     xTime = as.Date("2000-01-11")+((Times-1)/48-10),
+#     forecastPanels = NULL)
