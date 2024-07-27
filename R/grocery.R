bcbf5394bd13ce726d745113272e31239e0e5d92
Rob Hyndman <robjhyndman@gmail.com>
2024-07-27 17:14:08 +1000

    Avoid short urls

--- R/grocery.R
+++ R/grocery.R
@@ -11,7 +11,7 @@
 #' @keywords datasets
 #'
 #' @references Australian Bureau of Statistics, CAT 8501.0.
-#' (\href{http://tinyurl.com/hvx2vx2}{TABLE 11. Retail Turnover, State by Industry Subgroup,
+#' (\href{https://www.abs.gov.au/AUSSTATS/abs@.nsf/DetailsPage/8501.0Jun%202016?OpenDocument}{TABLE 11. Retail Turnover, State by Industry Subgroup,
 #' Original})
 #'
 #' @examples
@@ -20,3 +20,4 @@
 
 # @source \href{http://www.abs.gov.au/ausstats/meisubs.NSF/log?openagent&8501011.xls&8501.0&Time%20Series%20Spreadsheet&6D0A6496103804BACA2580040013D08C&0&Jun%202016&04.08.2016&Latest}
 # @source \href{http://www.abs.gov.au/AUSSTATS/abs@.nsf/DetailsPage/8501.0Jun%202016?OpenDocument}
+#
